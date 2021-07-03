8c05c44bd66b62ba08ec139a84ce940bdfb9fc46
avarnhagen <alex.m.varnhagen@vanderbilt.edu>
2021-07-03 15:53:38 -0400

    Election Analysis Update

--- PyPoll.py
+++ PyPoll.py
@@ -1,18 +1,18 @@
-#Open the data file.
 
-# Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Election_Analysis","Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("Election_Analysis","analysis", "election_analysis.txt")
 
-# Open the election results and read the file
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-     # To do: perform analysis.
-     print(election_data)
+# Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
 
-
-#Write down the names of all the candidates.
-#Add a vote count for each candidate.
-#Get the total votes for each candidate.
-#Get the total votes cast for the election.
-
-#Import the datetime class from the datetime module.
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
