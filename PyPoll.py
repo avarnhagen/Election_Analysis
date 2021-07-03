fef476445721167112ad5f614bb6ea4ee4710d10
avarnhagen <alex.m.varnhagen@vanderbilt.edu>
2021-07-03 15:13:51 -0400

    7-2 election analysis updates

---
+++ PyPoll.py
@@ -0,0 +1,18 @@
+#Open the data file.
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
+
+
+#Write down the names of all the candidates.
+#Add a vote count for each candidate.
+#Get the total votes for each candidate.
+#Get the total votes cast for the election.
+
+#Import the datetime class from the datetime module.
